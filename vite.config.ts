d4d0ed156b9e6dee60d977d841a704317b03c214
Carlos Padron <carlosap@zvomini.local>
2026-07-07 09:36:02 -0600

    Allow local dev host for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['zvomini.local'],
   },
   preview: {
     host: '0.0.0.0',
     port: 4173,
+    allowedHosts: ['zvomini.local'],
   },
 });
